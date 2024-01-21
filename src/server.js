e5592a22cb0efc41b9fc263f97b52a49f51e9f53
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-21 20:53:56 -0300

    creating query params

--- src/server.js
+++ src/server.js
@@ -7,8 +7,17 @@ app.get("/message/:id/:user", (request, response) => {
 
    response.send(` 
    Mensagem ID: ${request.params.id},
-   Para o usuário: ${request.params.user} `)
-})
+   Para o usuário: ${request.params.user} 
+   `);
+});
+
+app.get("/users", (request, response) => {
+    const {page, limit} = request.query;
+
+    response.send(
+        `Página: ${page}. Mostrar: ${limit}`
+    );
+});
 
 const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
