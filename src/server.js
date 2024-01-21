f66b67fe079462e4cc695aa7f24b22da8cbafdcb
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-21 20:24:30 -0300

    add /message/user id in sever.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express()
 
-app.get("/", (request, response) => {
+app.get("/message/:id", (request, response) => {
     response.send("HELLO WORD!")
 })
 
