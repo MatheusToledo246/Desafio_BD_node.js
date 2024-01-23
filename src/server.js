ed57c95ce5bd5491721ef183bf01ae2e8273c7c2
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-22 21:07:34 -0300

    install nodemon and create dev

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json());
 
 app.get("/message/:id/:user", (request, response) => {
    const { id, user} = request.params;
