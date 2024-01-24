51ccd4c2bfdc6ffc375c62c6a5e08a03669e16f5
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-23 22:47:40 -0300

    add folder utils and create AppError

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,18 +2,8 @@ const { Router } = require("express");
 const UsersController = require("../controllers/usersController")
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next){
-    console.log("Você passou pelo Middleware");
-
-    if ( !request.body.isAdmin){
-        return response.json({ message: "user unauthorized"});
-    }
-
-    next();
-}
-
 const usersController = new UsersController();
-usersRoutes.use(myMiddleware)
+
 usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
