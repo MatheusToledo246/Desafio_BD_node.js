c838e330cc6de4dd5fd10dcae6ca1a7079776f3b
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-23 22:22:28 -0300

    add middleware in users.routes to check user permission

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,7 +2,18 @@ const { Router } = require("express");
 const UsersController = require("../controllers/usersController")
 const usersRoutes = Router();
 
+function myMiddleware(request, response, next){
+    console.log("Você passou pelo Middleware");
+
+    if ( !request.body.isAdmin){
+        return response.json({ message: "user unauthorized"});
+    }
+
+    next();
+}
+
 const usersController = new UsersController();
+usersRoutes.use(myMiddleware)
 usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
