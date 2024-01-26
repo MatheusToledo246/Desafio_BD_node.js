c55426e480e75634868ef7dad3156d1b1c7fb5ba
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-25 22:17:15 -0300

    add SELECT in checkUsers

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,14 +1,18 @@
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const {name, email, password} = request.body;
 
-        if(!name){
-            throw new AppError("O nome é obrigatório")
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+       
+        if(checkUserExist){
+            throw new AppError("Este e-mail já esta em uso.");
         }
 
-        response.status(201).json({name, email, password})
+        return response.status(201).json();
     }
 }
 
