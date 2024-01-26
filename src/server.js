b2c89723190efee5efc7aa26ecc67ef08a819cfe
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-25 21:57:49 -0300

    add migrations to create automate the users  table

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 require("express-async-errors");
-const database = require("./database/sqlite")
-
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError");
+
 const express = require("express");
 const routes = require("./routes");
+
+migrationsRun();
 
 
 const app = express()
@@ -11,8 +13,6 @@ app.use(express.json());
 
 
 app.use(routes);
-database();
-
 app.use(( error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
