569d6f622114466fc3fd0dfa51d0700c7cbfe5c9
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-28 21:33:55 -0300

    create index for exibing notes register

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -46,6 +46,17 @@ await knex("tags").insert(tagsInsert);
         return response.json();
     }
 
+    async index(request, response) {
+        const { user_id } = request.query;
+
+        const notes = await knex("notes")
+        .where({ user_id })
+        .orderBy("title");
+
+        return response.json({ notes });
+        
+    }
+
 }
 
 module.exports = NotesController;
