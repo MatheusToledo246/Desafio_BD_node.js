d87af02f88a859819f4206c498c71c104f07c428
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-28 22:25:20 -0300

    Getting note tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -9,6 +9,7 @@ class NotesController{
     const [note_id] = await knex("notes").insert({
         title,
         description,
+        noteMovie,
         user_id
     });
 
@@ -55,7 +56,16 @@ await knex("tags").insert(tagsInsert);
             const filterTags = tags.split(',').map(tag => tag.trim());
             
             notes = await knex("tags")
+            .select([
+                "notes.id",
+                "notes.title",
+                "notes.user_id",
+            ])
+            .where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
             .whereIn("name", filterTags)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
         }else{
 
         notes = await knex("notes")
@@ -64,8 +74,17 @@ await knex("tags").insert(tagsInsert);
         .orderBy("title");
         
         }
-        
-        return response.json({ notes });
+
+        const userTags = await knex("tags").where({ user_id });
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+        return response.json({ notesWithTags });
         
     }
 
